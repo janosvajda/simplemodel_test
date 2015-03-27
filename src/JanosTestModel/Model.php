9a878da86dae2dba9dcd3601e06f2250bac9ee48
Janos Vajda <vajdajanos@gmail.com>
2015-03-27 15:05:56 +0000

    Update Model.php
    reformatted the code

--- src/JanosTestModel/Model.php
+++ src/JanosTestModel/Model.php
@@ -3,20 +3,13 @@ namespace JanosTestModel;
 
 class JanosTestModel_Test {
 
-public function __construct(){
-
-  echo "test";
-
-}
-
-public function test(){
-
-  var_dump("ok");
-
-}
-
-
-
+  public function __construct(){
+    echo "test";
+  }
+  
+  public function test(){
+    var_dump("ok");
+  }
 }
 
 
